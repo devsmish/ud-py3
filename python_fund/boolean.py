62bd17805ab3015a7693005359c6d16f22046835
devsmish <smyshynskyi@gmail.com>
2026-02-05 23:29:58 +0100

    add 3 files boolean.py, Python-Data-Types.png and sets.py

---
+++ python_fund/boolean.py
@@ -0,0 +1,67 @@
+# print(1 < 2)
+# print(type(True))
+# print(type(False))
+#
+# # Comparison operators
+#
+# print(1 == 1)
+# print(1 == 2)
+# print('Hello' == 'Hello')
+# print('Hello' == 'hello')
+
+# print(1 != 1)
+# print(1 != 2)
+# print('Hello' != 'Hello')
+# print('Hello' != 'hello')
+
+# print(1 > 2)
+# print(1 < 2)
+# print(2 >= 2)
+# print(3 >= 2)
+# print(2 <= 2)
+# print(3 <= 2)
+#
+# print(ord('a'))
+# print(ord('b'))
+# print('a' > 'b')
+# print('hi' > 'hello')
+# print(ord('i'))
+# print(ord('e'))
+
+# x = 10
+# y = 23
+#
+# print(x > y)
+# print(x < y)
+# print(x == y)
+# print(x != y)
+
+# age = int(input('Input your age: '))
+# print('Access is permitted: ' + str(age >= 18))
+
+# HOMEWORK
+'''Вопросы к этому заданию
+1. Создайте 2 переменных, содержащие числовые значения. Сравните их при помощи всех операторов сравнения и выведите 
+результат на экран
+
+2. Сравните две одинаковых буквы, но одна из них должна быть заглавной, при помощи операторов сравнения ">" и выведите 
+результат на экран. Выведите на экран ASCII код каждой из букв'''
+a = 5
+b = 7
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+a = 'A'
+b = 'a'
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(ord(a))
+print(ord(b))
